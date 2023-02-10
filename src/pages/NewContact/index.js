1a9c100493be018bf03a3f115e023428de2f52d6
barbara castro <barbara_castrosilva@hotmail.com>
2023-02-10 00:05:48 -0300

    create component input and select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,18 @@
 import PageHeader from '../../Components/PageHeader';
+import Input from '../../Components/Input';
+import Select from '../../Components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
